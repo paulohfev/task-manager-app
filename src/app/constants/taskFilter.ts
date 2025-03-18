8d0cc72682e385596bf46dea989a8952e1c9c26f
paulohfev <phfernandesev@gmail.com>
2025-03-18 18:23:10 -0300

    Add toggle buttons for filtering list of tasks

---
+++ src/app/constants/taskFilter.ts
@@ -0,0 +1,5 @@
+export enum TaskFilter {
+  all = "all",
+  completed = "completed",
+  incomplete = "incomplete",
+};
--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,21 +1,39 @@
-import { useEffect } from "react";
-import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Box, ToggleButton, ToggleButtonGroup } from "@mui/material";
 import TaskItem from "../TaskItem";
+import { TaskFilter } from "@/app/constants/taskFilter";
 import { useAppDispatch, useAppSelector } from "@/store/store";
 import { fetchTasks } from "@/store/slices/tasksSlice";
 import styles from './styles';
 
 const TaskList: React.FC = () => {
   const dispatch = useAppDispatch();
+  const [filter, setFilter] = useState<TaskFilter>(TaskFilter.all);
 
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
   const tasks = useAppSelector((state) => state.tasks?.list);
 
+  const filteredTasks = tasks?.filter((task) => {
+    if (filter === TaskFilter.completed) return task.completed;
+    if (filter === TaskFilter.incomplete) return !task.completed;
+    return true;
+  });
+
   return (
     <Box sx={styles.wrapper}>
-      {tasks?.map((task) => (
+      <ToggleButtonGroup
+        value={filter}
+        exclusive
+        onChange={(_e, newFilter) => newFilter && setFilter(newFilter)}
+      >
+        <ToggleButton value={TaskFilter.all}>All</ToggleButton>
+        <ToggleButton value={TaskFilter.completed}>Completed</ToggleButton>
+        <ToggleButton value={TaskFilter.incomplete}>Incomplete</ToggleButton>
+      </ToggleButtonGroup>
+
+      {filteredTasks?.map((task) => (
         <TaskItem key={`task-${task.id}`} task={task} />
       ))}
     </Box>
